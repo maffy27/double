99beee71a315e405943fb56ab6faf840c4885569
maffy <maffy27@mail.ru>
2020-07-03 16:09:47 +0300

    firtcommit

---
+++ src/app/double/double.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-double',
+  templateUrl: './double.component.html',
+  styleUrls: ['./double.component.scss']
+})
+export class DoubleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
